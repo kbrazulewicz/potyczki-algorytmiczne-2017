918cd106633ae9778464cf03d71571b3f5e776ec
Krystian Brazulewicz <krystian@brazulewicz.pl>
2017-11-21 08:36:21 +0100

    1.A.Permutacja

--- 1.A.Permutacja/problem.cpp
+++ 1.A.Permutacja/problem.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 using namespace std;
 
 #define DEBUG
@@ -8,7 +9,25 @@ using namespace std;
 
 #define FOR(E, I) for(auto E = I.begin(); E != I.end(); E++)
 
+const int MAX_N = 250000;
+const unsigned long long MAX_K = 1000000000000000000ull;
+
 int main()
 {
+	unsigned long long n;
+	unsigned long long k;
+	cin >> n >> k;
+
+	cout << n << " " << k << " " << k * n << endl;
+
+	// test if n generates even number of pairs 
+	unsigned long long numberOfPairs = n * (n - 1) / 2;
+	if (n % 2 != 0) {
+		cout << "NIE" << endl;
+	}
+
+	unsigned long long numberOfInversions = n / 2;
+
+	cout << n << " " << k << endl;
 	return 0;
 }
